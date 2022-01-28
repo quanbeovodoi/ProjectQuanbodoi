ab6ed59b3cd055f20b0c638aa25eb5137bd5dbc3
quanbeovodoi <quanbeovodoi@gmail.com>
2022-01-28 21:47:15 +0700

    update

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAtXKWmFX7RcdKQo3n4jhrfRGYGqLqSfJE",
+    authDomain: "eccomerceweb.firebaseapp.com",
+    projectId: "eccomerceweb",
+    storageBucket: "eccomerceweb.appspot.com",
+    messagingSenderId: "847721028338",
+    appId: "1:847721028338:web:90ddad45d0a24897ecce6b",
+    measurementId: "G-LX4SRHWYMF"
+};
+export default firebaseConfig;
